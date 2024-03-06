0ed5d17c6f23cbc8389d6c59aa1bb8864149a4e9
Enzo Kayser <e.kayser@inetcons.it>
2024-03-06 19:17:41 +0100

    pass hiden

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
 module.exports = {
   APP_PORT: 3000,
-  DB_HOST: 'sql-server-tests.database.windows.net',
-  DB_PORT: 1433,
-  DB_USER: 'owner',
-  DB_PASSWORD: '3giKQhy68aGu82C',
-  DB_NAME: 'db',
 }
